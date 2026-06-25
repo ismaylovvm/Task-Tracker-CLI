75f50816174fd827f2463e920ef2494972f1c69d
Mahammad <ismayilzade.mhmmdd@gmail.com>
2026-06-25 13:39:28 +0300

    adding new add and list commands

--- tasl_cli.py
+++ tasl_cli.py
@@ -43,6 +43,101 @@ class Task:
 
 
 
+# komut kontrolu
+
+if argümanlar[1] == "add":
+    
+    gorev_metni = argümanlar[2]
+
+    #Yeni görevleri eklemek için mevcut görev listesini getirme
+
+    mevcut_gorevler = file_operations.gorevleri_getir()
+
+    #Uygun id bulmak için görev listesinde son kalınan yer
+
+    id = len(mevcut_gorevler) + 1
+
+    # Görevi json formatında kullanmak için yapı
+
+    yeni_gorev_nesnesi = Task(id,gorev_metni)
+    yeni_gorev = yeni_gorev_nesnesi.to_dict()
+
+    # Görevi mevcut görevlerin üzerine yaz
+
+    mevcut_gorevler.append(yeni_gorev)
+
+    # Görev listesine ekle
+
+    file_operations.gorevleri_kaydet(mevcut_gorevler)
+
+
+
+
+
+
+elif argümanlar[1] == "list":
+
+    # Farklı durumlara göre listeleme
+
+    if len(argümanlar) < 3:
+        print("Lutfen komutlari dogru giriniz")
+        sys.exit(1)
+
+
+    mevcut_gorevler = file_operations.gorevleri_getir()
+
+
+    if argümanlar[2] == "done":
+
+        for gorev in mevcut_gorevler:
+            if gorev["status"] == "done":
+                print(f"ID: {gorev['id']} | Durum: {gorev['status']} | Görev: {gorev['description']}")
+
+    
+
+    elif argümanlar[2] == "todo":
+    
+        for gorev in mevcut_gorevler:
+            if gorev["status"] == "todo":
+                print(f"ID: {gorev['id']} | Durum: {gorev['status']} | Görev: {gorev['description']}")
+
+    
+    elif argümanlar[2] == "in-progress":
+
+        for gorev in mevcut_gorevler:
+            if gorev["status"] == "in-progress":
+                print(f"ID: {gorev['id']} | Durum: {gorev['status']} | Görev: {gorev['description']}")
+
+
+    else:
+
+        for gorev in mevcut_gorevler:
+                print(f"ID: {gorev['id']} | Durum: {gorev['status']} | Görev: {gorev['description']}")
+
+    
+
+
+
+
+
+
+       
+
+
+elif argümanlar[1] == "update":
+    print("güncelleme işlemi yapilacak")
+
+    
+elif argümanlar[1] == "delete":
+    print("Silme işlemi yapilacak")
+
+else:
+    print("Bilinmeyen komut girildi")
+
+    
+
+
+
 
 
 
